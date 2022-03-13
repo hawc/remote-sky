80a1404ab01c3348e2ca0e5b73119f1964366b5b
Hendrik Wichern <git@hendrikwichern.de>
2022-03-13 19:05:24 +0100

    added ssl config

--- server/peerServer.js
+++ server/peerServer.js
@@ -1,14 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const { ExpressPeerServer } = require('peer');
+const path = require('path');
+const fs = require('fs');
 
 const server = app.listen(9001);
 
+let privateKey;
+let certificate;
+let credentials;
+
+if (process.env.NODE_ENV === 'production') {
+    const certs = '/etc/letsencrypt/live/remote-sky.hawc.de/';
+
+    privateKey = fs.readFileSync(path.join(certs, 'privkey.pem'));
+    certificate = fs.readFileSync(path.join(certs, 'cert.pem'));
+
+    credentials = {
+        key: privateKey,
+        cert: certificate
+    };
+}
+
 const peerServer = ExpressPeerServer(server, {
     proxied: true,
-    debug: true,
+    debug: process.env.NODE_ENV !== 'production',
     path: '/myapp',
-    secure: false,
+    ssl: process.env.NODE_ENV === 'production' ? credentials : {},
 });
 
 app.use(peerServer);
